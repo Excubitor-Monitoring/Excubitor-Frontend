f6d78f2db7e6f20413ebe9db6815d08eaa87c551
Kay Knöpfle <35371434+Joystick01@users.noreply.github.com>
2023-05-26 14:16:11 +0200

    removed style loading (feature dropped)

--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -11,13 +11,6 @@
 		else{
 			$host_config.modules.forEach(module => {
 				import(`http://${$host}:8000/${module.js}`)
-				if(module.css != undefined){
-
-					let link = document.createElement("link");
-					link.rel  = 'stylesheet';
-					link.href = `http://${$host}:8000/${module.css}`
-					document.getElementById("head")?.appendChild(link);
-				}
 			});
 
 		}
