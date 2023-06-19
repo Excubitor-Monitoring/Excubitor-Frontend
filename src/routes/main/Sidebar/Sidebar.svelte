fd06d2a3c146a806942fb29d451de5e9e317af2a
Kay Knöpfle <35371434+Joystick01@users.noreply.github.com>
2023-06-19 11:43:15 +0200

    added logo

--- src/routes/main/Sidebar/Sidebar.svelte
+++ src/routes/main/Sidebar/Sidebar.svelte
@@ -2,9 +2,11 @@
     import { host_config } from "/src/stores";
 	import SidebarItem from "./SidebarItem/SidebarItem.svelte";
 	import SidebarCategory from "./SidebarCategory/SidebarCategory.svelte";
+    import logo from "./logo.svg"
 </script>
 
 <div class="sidebar">
+    <img src={logo} alt="" class="w-1/3 py-5">
     {#each $host_config.modules as module}
         <SidebarCategory module={module}/>
     {/each}
@@ -14,6 +16,6 @@
     .sidebar {
         @apply flex flex-col items-center;
         @apply min-w-[15%] min-h-full overflow-hidden text-gray-400 bg-gray-900;
-        @apply p-2;
+        @apply px-5;
     }
 </style>
